3b71d9de849e926bbde6f06d48ff57aa73bd70dd
Rusley Arthus <rusleyarthus@gmail.com>
2024-05-05 18:41:16 -0300

    Adicionando Tratamento de Requisições não Encontradas

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-import tabela from "./tabela.js";
+import tabela from './tabela.js';
 import express from 'express';
 
 const app = express();
 
-app.get("/", (requisicao, resposta) => {
-    resposta.send(tabela);
+app.get('/', (requisicao, resposta) => {
+    resposta.status(200).send(tabela);
 });
 
-app.get("/:sigla", (requisicao, resposta) => {
+app.get('/:sigla', (requisicao, resposta) => {
     const siglaInformada = requisicao.params.sigla.toUpperCase();
     const time = tabela.find((infoTime) => infoTime.sigla === siglaInformada);
-    resposta.send(time);
+    if(!undefined){
+        resposta.status(404).send('Não existe na série A do Brasileirão um time com a sigla informada!');
+        return;
+    }
+    resposta.status(200).send(time);
 });
 
 app.listen(8080, () => console.log('Servidor Funcionando!'));
