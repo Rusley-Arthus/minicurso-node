0d52f7a039fbe28166de9baad773c0b73cce6238
Rusley Arthus <rusleyarthus@gmail.com>
2024-05-05 16:09:08 -0300

    Adicionando Endpoint sigla

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express();
 
 app.get("/", (requisicao, resposta) => {
     resposta.send(tabela);
-})
+});
+
+app.get("/:sigla", (requisicao, resposta) => {
+    const siglaInformada = requisicao.params.sigla.toUpperCase();
+    const time = tabela.find((infoTime) => infoTime.sigla === siglaInformada);
+    resposta.send(time);
+});
 
 app.listen(8080, () => console.log('Servidor Funcionando!'));
